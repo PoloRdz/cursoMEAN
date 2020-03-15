1229e51fcb25e3481121d92a6c54e13fa2daadbf
PoloRdz <prhiroal@gmail.com>
2020-03-15 14:43:50 -0600

    Mejoras a la app
    Cambios a song.js y user.js

--- controllers/song.js
+++ controllers/song.js
@@ -8,7 +8,10 @@ var songModel = require('../models/song');
 function getSong(req, res){
     var songId = req.params.id;
 
-    songModel.findById(songId).populate({path: 'album'}).exec((err, song) => {
+    songModel.findById(songId).populate({path: 'album',
+    populate: {
+        path: 'artist',
+        model: 'Artist'}).exec((err, song) => {
         if(err){
             res.status(500).send({message: 'Error en la petición'});
         } else{
@@ -22,14 +25,20 @@ function getSong(req, res){
 }
 
 function getSongs(req, res){
-    var songId = req.params.artist;
+    var albumId = req.params.album;
 
-    if(!songId){
+    if(!albumId){
         var find = songModel.find({}).sort('number');
     } else {
-        var find = songModel.find({artist: songId}).sort('name');
+        var find = songModel.find({album: albumId}).sort('number');
     }
-    find.populate({path: 'album'}).exec((err, songs) => {
+    find.populate({
+        path: 'album',
+        populate: {
+            path: 'artist',
+            model: 'Artist'
+        }
+    }).exec((err, songs) => {
         if(err){
             res.status(500).send({message: 'Error en la peticion'});
         } else{
@@ -103,13 +112,13 @@ function uploadFile(req, res){
 
     if(req.files){
 
-        var file_path = req.files.image.path;
+        var file_path = req.files.file.path;
         var file_split = file_path.split('\\');
         var file_name = file_split[2]; 
 
         var ext_split = file_name.split('\.');
         var file_ext = ext_split[1];
-        if(file_ext == 'mp3'){
+        if(file_ext == 'mp3' || file_ext == 'ogg'){
             songModel.findByIdAndUpdate(songId, {file: file_name}, (err, songUpdated) => {
                 if(!songUpdated){
                     res.status(404).send({message: 'No se ha podido actualizar la canción'});
