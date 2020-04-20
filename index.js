4d0bab97842959c1cab7c3433e407889c90434ab
PoloRdz <prhiroal@gmail.com>
2020-04-20 16:46:59 -0600

    3977

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require("./app");
-var port = process.env.port || 8080;
+var port = process.env.port || 3977;
 
 mongoose.connect('mongodb+srv://apiUsr:Amonos123@cluster0-ycxpl.mongodb.net/test?retryWrites=true&w=majority', (err, res) =>{
     if(err){
